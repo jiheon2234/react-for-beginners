fbdf27818f22ae7f3732df9a71e5663becedba2a
jiheon Kim <jiheon2234@gmail.com>
2022-08-31 15:16:18 +0900

    lec6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hi:)");
+    return function () {
+      console.log("bye:(");
+    };
+  }, []);
+  // useEffect(function () {
+  //   console.log("hi:)");
+  //   return function () {
+  //     console.log("bye:(");
+  //   };
+  // }, []);
+
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
     </div>
   );
 }
+
+// function App() {
+//   const [counter, setValue] = useState(0);
+//   const [keyword, setKeyword] = useState("");
+//   const onClick = () => setValue((prev) => prev + 1);
+//   const onChange = (event) => setKeyword(event.target.value);
+//   // console.log("i run all the time");
+//   useEffect(() => {
+//     console.log("I run only once");
+//   }, []); //처음에만실행됨
+//   useEffect(() => {
+//     if (keyword !== "" && keyword.length > 6) {
+//       console.log("I run Keyword changes", keyword);
+//     }
+//   }, [keyword]);
+
+//   useEffect(() => {
+//     console.log("I run when 'counter' changes");
+//   }, [counter]);
+
+//   useEffect(() => {
+//     console.log("i run when counter | keyword changes");
+//   }, [counter, keyword]);
+
+//   return (
+//     <div>
+//       <input value={keyword} onChange={onChange} type="text" placeholder="Search here...." />
+//       <h1>{counter}</h1>
+//       <button onClick={onClick}>Click me!!</button>
+//     </div>
+//   );
+// }
 
 export default App;
