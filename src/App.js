2c17d760985b7d4e048a69c0abe140ae91af2073
jiheon Kim <jiheon2234@gmail.com>
2022-08-31 21:00:08 +0900

    튜토리얼 끝

--- src/App.js
+++ src/App.js
@@ -1,63 +1,29 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi:)");
-    return function () {
-      console.log("bye:(");
-    };
-  }, []);
-  // useEffect(function () {
-  //   console.log("hi:)");
-  //   return function () {
-  //     console.log("bye:(");
-  //   };
-  // }, []);
-
-  return <h1>Hello</h1>;
-}
+import { useEffect, useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [loading, setLoading] = useState("true");
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      <h1>The coins ({loading ? null : coins.length})</h1>
+      {loading ? <strong>Loading.....</strong> : null}
+      <u1>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </u1>
     </div>
   );
 }
-
-// function App() {
-//   const [counter, setValue] = useState(0);
-//   const [keyword, setKeyword] = useState("");
-//   const onClick = () => setValue((prev) => prev + 1);
-//   const onChange = (event) => setKeyword(event.target.value);
-//   // console.log("i run all the time");
-//   useEffect(() => {
-//     console.log("I run only once");
-//   }, []); //처음에만실행됨
-//   useEffect(() => {
-//     if (keyword !== "" && keyword.length > 6) {
-//       console.log("I run Keyword changes", keyword);
-//     }
-//   }, [keyword]);
-
-//   useEffect(() => {
-//     console.log("I run when 'counter' changes");
-//   }, [counter]);
-
-//   useEffect(() => {
-//     console.log("i run when counter | keyword changes");
-//   }, [counter, keyword]);
-
-//   return (
-//     <div>
-//       <input value={keyword} onChange={onChange} type="text" placeholder="Search here...." />
-//       <h1>{counter}</h1>
-//       <button onClick={onClick}>Click me!!</button>
-//     </div>
-//   );
-// }
 
 export default App;
